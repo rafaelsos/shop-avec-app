e6376cc8caa780b19da256a77806c4f411e6279b
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-04-06 15:41:41 -0300

    estilizada pages/Cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,11 +1,116 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Total } from './styles';
+
+interface Cart {
+  id: number;
+  picture: string;
+  description: string;
+  title: string;
+  price: number;
+  memory: string;
+  brand: string;
+  priceFormatted: string;
+  amount: number;
+}
 
 export default function Cart() {
+  const cartProducts = [
+    {
+      id: 133453126,
+      title:
+        'Smartphone Motorola Moto G6 Dual Chip Android Oreo - 8.0 Tela 5.7" Octa-Core 1.8 GHz 32GB 4G Câmera 12 + 5MP (Dual Traseira) - Índigo',
+      price: 1299,
+      picture:
+        'https://imagens.canaltech.com.br/produto/buscape/o292298908.jpg',
+      description:
+        'Misturando inovação, modernidade e qualidade, o novo moto g6 chegou para deixar tudo o que você precisa na palma da sua mão, desbravando um mundo de novidades e experiências extremamente inesquecíveis. Com processador octa-core de 1,8 GHz, 3GB de RAM(1), bateria que dura o dia todo(2) e carregamento TurboPower™, você executa suas tarefas sem se preocupar(3). Além disso, sua deslumbrante Tela Max Vision permite com que você veja sua vida em tela cheia, já que pode assistir a tudo em um incrível espaço Full HD+ de 5,7 polegadas. Pensa que acabou? Então prepare-se: este novo integrante da Família moto g6 possui design com acabamento traseiro em vidro 3D que se encaixa perfeitamente em suas mãos.E não para por aí! Agora você pode registrar todos os seus momentos em fotos incríveis, já que o moto g6 vem com câmera traseira dupla e câmera frontal de 8MP com flash LED. Ah, e ele tem Android Puro 8.0 Oreo, a última versão do sistema operacional mais utilizado em todo o mundo. moto g6: impressionante em todos os ângulos.',
+      memory: '32GB',
+      brand: 'Motorola',
+      chipType: 'Nano Chip',
+      quantity: 2,
+    },
+    {
+      id: 132537782,
+      title:
+        'Smartphone Samsung Galaxy J7 Pro Android 7.0 Tela 5.5" Octa-Core 64GB 4G Wi-Fi Câmera 13MP - Preto',
+      price: 1229,
+      picture:
+        'https://imagens.canaltech.com.br/produto/buscape/o268909836.jpg',
+      description:
+        'O Smartphone Samsung Galaxy J7 Pro possui tela de 5.5 polegadas com a tecnologia Super Amoled, isso permite que você visualize fotos, vídeo, jogos e seus aplicativos preferidos com cores mais intensas e maior nitidez. Entre seus destaques estão suas duas câmeras, traseira e frontal de 13MP, ambas com Flash LED para você tirar fotos com alta qualidade mesmo em ambientes de baixa luminosidade. Além disso, é possível controlar o obturador com um simples gesto manual. O Galaxy J7 Pro também conta com processador Octa Core de última geração, capaz de rodar seus aplicativos com velocidades de tirar o fôlego. Aliado a uma memória RAM de 3GB, oferendo uma experiência móvel de alta qualidade para você poder fazer streaming de vídeos, jogar os mais recentes games ou ver suas séries favoritas com o melhor desempenho. Equipado com uma bateria para não te deixar na mão e poder usar o telefone sem preocupações. Porém ele conta com uma solução inteligente para economizar tempo e bateria. A Tela Always On permite que você acesse a hora, o calendário e as notificações recebidas rapidamente sem precisar ativar o aparelho. Este celular vem com 64GB de armazenamento interno, mas caso não seja suficiente você poderá expandir essa capacidade através de um cartão microSD de até 256GB (vendido separadamente). Outra grande novidade do Galaxy J7 Pro é que ele agora possui sensor de impressões digitais, provendo a máxima proteção de seus dados e garantindo que somente você os acesse.',
+      memory: '64GB',
+      brand: 'Samsung',
+      chipType: 'Nano Chip',
+      quantity: 4,
+    },
+  ];
+
   return (
     <Container>
-      <h1>Cart</h1>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>PRODUTO</TableCell>
+            <TableCell>DETALHE</TableCell>
+            <TableCell>QTD</TableCell>
+            <TableCell>SUBTOTAL</TableCell>
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cartProducts.map((item) => (
+            <TableRow key={String(item.id)}>
+              <TableCell component="th" scope="row">
+                <img src={item.picture} alt="" />
+              </TableCell>
+              <TableCell>
+                <strong>{item.title}</strong>
+                <span>{item.price}</span>
+              </TableCell>
+              <TableCell>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#4b195f" />
+                  </button>
+                  <input type="number" readOnly value="111" />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#4b195f" />
+                  </button>
+                </div>
+              </TableCell>
+              <TableCell>
+                <strong>{item.price}</strong>
+              </TableCell>
+              <TableCell>
+                <button type="button">
+                  <MdDelete size={25} color="#4b195f" />
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$999,00</strong>
+        </Total>
+      </footer>
     </Container>
   );
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,3 +1,124 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 10px;
+  background: #fff;
+
+  table {
+    width: 100%;
+
+    thead th {
+      color: #999;
+      text-align: left;
+      padding: 12px;
+    }
+
+    tbody td {
+      padding: 12px;
+      border-bottom: 1px solid #eee;
+    }
+
+    img {
+      height: 100px;
+    }
+
+    strong {
+      color: #333;
+      display: block;
+    }
+
+    span {
+      margin-top: 5px;
+      font-size: 18px;
+      font-weight: bold;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      background: #f5f5f5;
+
+      input {
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        color: #666;
+        padding: 6px;
+        width: 50px;
+      }
+    }
+
+    button {
+      background: none;
+      border: 0;
+      padding: 6px;
+    }
+
+    @media all and (max-width: 625px) {
+      img {
+        height: 70px;
+      }
+
+      strong {
+        font-size: 12px;
+      }
+
+      span {
+        font-size: 12px;
+      }
+
+      div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        input {
+          width: 45px;
+        }
+      }
+
+      svg {
+        width: 25px;
+      }
+    }
+  }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 30px;
+
+    button {
+      background: #4b195f;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#4b195f')};
+      }
+    }
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
